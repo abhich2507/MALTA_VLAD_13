44ccf1aa916d3feac3b311fe69f5ba591b6033c8
Vlad Dumitru Berlea <vlad.berlea@desy.de>
2025-07-07 17:13:23 +0200

    minor fix

--- condor_submission/run_script_naf.sh
+++ condor_submission/run_script_naf.sh
@@ -2,5 +2,5 @@ source ../config.sh
 
 source "$NAF_GEANT"
 #export NUM_CORES=2
-export SIMU_CONFIG="/afs/desy.de/user/"${NAF_USER:0:1}"/$NAF_USER/$NAF_PATH"/flags.cfg
-"/afs/desy.de/user/"${NAF_USER:0:1}"/$NAF_USER/$NAF_PATH"/build/sim 
+export SIMU_CONFIG=/afs/desy.de/user/${NAF_USER:0:1}/$NAF_USER/$NAF_PATH/flags.cfg
+/afs/desy.de/user/${NAF_USER:0:1}/$NAF_USER/$NAF_PATH/build/sim
