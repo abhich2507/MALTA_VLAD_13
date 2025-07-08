5f4965346aa9045e4093ee2484252adde6a14c1f
Vlad Dumitru Berlea <vlad.berlea@desy.de>
2025-07-08 09:41:33 +0200

    minor change

--- condor_submission/run_script_naf.sh
+++ condor_submission/run_script_naf.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-set -e
 
 source /afs/desy.de/user/b/berleavl/private/decal_sw_cern/config.sh
+set -e
 
 echo "NAF_USER=$NAF_USER"
 echo "NAF_PATH=$NAF_PATH"
 echo "NAF_GEANT=$NAF_GEANT"
 
+echo "SIM_PATH=/afs/desy.de/user/${NAF_USER:0:1}/$NAF_USER/$NAF_PATH/build/sim"
 source "$NAF_GEANT"
 export SIMU_CONFIG=/afs/desy.de/user/${NAF_USER:0:1}/$NAF_USER/$NAF_PATH/flags.cfg
 /afs/desy.de/user/${NAF_USER:0:1}/$NAF_USER/$NAF_PATH/build/sim
--- config.sh
+++ config.sh
@@ -2,6 +2,6 @@ export LOCAL_PATH=/home/vlad/Documents/Simu/Geant4/DECAL_REPO/
 export LOCAL_GEANT=/home/vlad/Documents/Geant4/geant4-v11.3.0/install/bin/geant4.sh
 export LOCAL_ROOT=/home/vlad/Documents/root/root-install/bin/thisroot.sh
 export EXTRA_LOCAL=LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu
-export NAF_GEANT=afs/desy.de/user/b/berleavl/private/geant4-install/bin/geant4.sh
+export NAF_GEANT=/afs/desy.de/user/b/berleavl/private/geant4-install/bin/geant4.sh
 export NAF_PATH=private/decal_sw_cern
 export NAF_USER=berleavl
