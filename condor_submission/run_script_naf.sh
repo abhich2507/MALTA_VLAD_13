454115f68dc18ce836bdf3f0197be31ea1ae2ff7
Vlad Dumitru Berlea <vlad.berlea@desy.de>
2025-07-08 09:12:25 +0200

    minor change

--- condor_submission/run_script_naf.sh
+++ condor_submission/run_script_naf.sh
@@ -1,6 +1,9 @@
-source /afs/desy.de/user/b/berleavl/private/decal_sw_cern/config.sh
+#source /afs/desy.de/user/b/berleavl/private/decal_sw_cern/config.sh
 
-source "$NAF_GEANT"
+#source "$NAF_GEANT"
+#export SIMU_CONFIG=/afs/desy.de/user/${NAF_USER:0:1}/$NAF_USER/$NAF_PATH/flags.cfg
+#/afs/desy.de/user/${NAF_USER:0:1}/$NAF_USER/$NAF_PATH/build/sim
+source /afs/desy.de/user/b/berleavl/private/geant4-install/bin/geant4.sh
 #export NUM_CORES=2
-export SIMU_CONFIG=/afs/desy.de/user/${NAF_USER:0:1}/$NAF_USER/$NAF_PATH/flags.cfg
-/afs/desy.de/user/${NAF_USER:0:1}/$NAF_USER/$NAF_PATH/build/sim
+export SIMU_CONFIG=/afs/desy.de/user/b/berleavl/private/decal_sw_cern/flags.cfg
+/afs/desy.de/user/b/berleavl/private/decal_sw_cern/build/sim 
