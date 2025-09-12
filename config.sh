4c727936a60c074c311878d91854f0853c633aef
Lucian Fasselt <lucian.fasselt@desy.de>
2025-09-12 11:53:21 +0200

    bringing develop up to date
    	EffMapCreation/MPV/250127_MPV_CZ_data/W12R7IBIAS03SUB10/
    
    Fixing conflicts to merge

--- config.sh
+++ config.sh
@@ -1,7 +1,7 @@
-export LOCAL_PATH=/home/vlad/Documents/Simu/Geant4/DECAL_REPO/
-export LOCAL_GEANT=/home/vlad/Documents/Geant4/geant4-v11.3.0/install/bin/geant4.sh
-export LOCAL_ROOT=/home/vlad/Documents/root/root-install/bin/thisroot.sh
+export LOCAL_PATH=/Users/lucianfasselt/DECAL/Simulation/Geant4/MALTASIM/malta_simulation/
+export LOCAL_GEANT=/Users/lucianfasselt/DECAL/Simulation/geant4-v11.3.0-install/bin/geant4.sh
+#export LOCAL_ROOT=/home/vlad/Documents/root/root-install/bin/thisroot.sh
 export EXTRA_LOCAL=LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu
-export NAF_GEANT=/afs/desy.de/user/b/berleavl/private/geant4-install/bin/geant4.sh
+export NAF_GEANT=/afs/desy.de/user/f/fasseltl/private/geant4-install/bin/geant4.sh
 export NAF_PATH=private/decal_sw_cern
 export NAF_USER=berleavl 
