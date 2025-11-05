11496fd11269eb47c095c95e5c79d98677e8be59
Vlad Dumitru Berlea <vlad.berlea@desy.de>
2025-11-05 15:38:43 +0100

    Reverting back to normal

--- run_files/run_script_local.sh
+++ run_files/run_script_local.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 if [ $HOME = "/home/vlad" ]; then
+    echo "Running as user vlad"
     source ../configs/config_vlad.sh
     source "$LOCAL_GEANT"
     source "$LOCAL_ROOT"
     export $EXTRA_LOCAL
 
 elif [$HOME = "/Users/lucianfasselt"]; then
+    echo "Running as user lucian"
     source ../configs/config_lucian.sh
 else 
+    echo "REVERTING TO ALMA9 cvmfs sourcing"
     source ../configs/config.sh
 fi
 FLAG=$1
